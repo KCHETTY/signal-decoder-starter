922bb3c7d2441a3c2c01bc61fefe71fb0dc0e95c
Keshan <kchetty416@gmail.com>
2026-02-20 07:41:48 +0200

    Updated Signal Controller validation

--- src/SignalDecoder.Api/Controllers/SignalController.cs
+++ src/SignalDecoder.Api/Controllers/SignalController.cs
@@ -38,6 +38,21 @@ namespace SignalDecoder.Api.Controllers
         [Route("decode")]
         public IActionResult Decodedevices([FromBody]DecodeRequest _Request)
         {
+            int SignalLength = _Request.Devices.First().Value.Length;
+
+            foreach (KeyValuePair<string, int[]> Device in _Request.Devices)
+            {
+                if (SignalLength != Device.Value.Length)
+                {
+                    return BadRequest("All signal patterns must have the same length.");
+                }
+
+                if (!Device.Value.All(v => v >= 0))
+                {
+                    return BadRequest("All signal patterns must contain non negative numbers.");
+                }
+            }
+            
             return Ok(_SignalDecoderService.Decode(_Request));
         }
     }
